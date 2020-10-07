d501999ca3e9bb28eda2d4f4d2e1563d609b4bae
sonikku10 <sonikku10@msn.com>
2020-10-06 20:20:53 -0400

    Update port listen message.

--- app.js
+++ app.js
@@ -109,5 +109,5 @@ if (port == null || port == "") {
 }
 
 app.listen(port, function() {
-  console.log("Server started successfully.");
+  console.log("Server started successfully on port " + port + ".");
 });
